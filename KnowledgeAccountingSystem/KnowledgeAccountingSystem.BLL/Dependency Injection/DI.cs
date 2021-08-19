baa6c7426c1eef17ab1d20eefad2742ec5f4369d
SalabayKostya <extrimdysik@gmail.com>
2021-08-19 17:31:42 +0300

    Create DI class and add connectionString

---
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Dependency Injection/DI.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnowledgeAccountingSystem.BLL.Dependency_Injection
+{
+    public static class DI
+    {
+        public static void AddDependency(this IServiceCollection services, string connectionString)
+        {
+
+        }
+    }
+}
--- KnowledgeAccountingSystem/KnowledgeAccountingSystem/Startup.cs
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using KnowledgeAccountingSystem.BLL.Dependency_Injection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,8 @@ namespace KnowledgeAccountingSystem
 
         public void ConfigureServices(IServiceCollection services)
         {
-
+            string connectionString = Configuration.GetConnectionString("DefaultConnectionString");
+            services.AddDependency(connectionString);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
