a08a7660f36887fd1a419b24f1499004084c1559
SalabayKostya <extrimdysik@gmail.com>
2021-08-21 19:03:12 +0300

    Create IAccountService interface

---
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using KnowledgeAccountingSystem.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeAccountingSystem.BLL.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<string> LoginAsync(string email, string password);
+        Task RegistrationAsync(UserModel model);
+        Task UpdateAccountAsync(UserModel model);
+        Task DeleteAccountAsync(int id);
+        int GetRoleId(int userId);
+    }
+}
