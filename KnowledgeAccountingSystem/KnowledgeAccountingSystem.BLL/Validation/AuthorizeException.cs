32a74703b6fe5ed0084109c1437eb330d5bb7115
SalabayKostya <extrimdysik@gmail.com>
2021-08-21 18:56:10 +0300

    Add CustomException classes

---
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Validation/AuthorizeException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace KnowledgeAccountingSystem.BLL.Validation
+{
+    public class AuthorizeException : KnowledgeAccountException
+    {
+        public AuthorizeException()
+        {
+
+        }
+
+        public AuthorizeException(string message) : base(message)
+        {
+
+        }
+        public AuthorizeException(string message, HttpStatusCode statusCode) : base(message, statusCode)
+        {
+
+        }
+
+        public AuthorizeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
---
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Validation/KnowledgeAccountException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace KnowledgeAccountingSystem.BLL.Validation
+{
+    [Serializable]
+    public class KnowledgeAccountException : Exception
+    {
+        protected readonly HttpStatusCode code;
+        public KnowledgeAccountException() : base()
+        {
+
+        }
+
+        public KnowledgeAccountException(string message) : base(message)
+        {
+
+        }
+        public KnowledgeAccountException(string message, HttpStatusCode statusCode) : base(message)
+        {
+            code = statusCode;
+        }
+
+        public KnowledgeAccountException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+
+    }
+}
