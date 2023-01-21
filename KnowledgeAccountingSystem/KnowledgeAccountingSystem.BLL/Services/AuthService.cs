17af0472f2608b83c1e485e137848267562c2c98
GhostDragon <44135388+NapadiyOles@users.noreply.github.com>
2023-01-21 20:34:11 +0200

    update

--- KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Services/AuthService.cs
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Services/AuthService.cs
@@ -107,4 +107,93 @@ namespace KnowledgeAccountingSystem.BLL.Services
             }
         }
     }
+
+    public class CopyOfAuthService : IAuthService
+    {
+        private readonly IUnitOfWork context;
+        private readonly IMapper mapper;
+        private readonly IConfiguration configuration;
+        private readonly IOptions<AuthOptions> authOptions;
+        public CopyOfAuthService(IUnitOfWork context, IMapper mapper, IConfiguration configuration, IOptions<AuthOptions> authOptions)
+        {
+            this.configuration = configuration;
+            this.context = context;
+            this.mapper = mapper;
+            this.authOptions = authOptions;
+            AddDefaultAdmin().Wait();
+        }
+
+        private string Encrypt(string password)
+        {
+            var data = Encoding.Unicode.GetBytes(password);
+            var encrypted = ProtectedData.Protect(data, null, DataProtectionScope.LocalMachine);
+            return Convert.ToBase64String(encrypted);
+        }
+
+        private string Decrypt(string password)
+        {
+            var data = Convert.FromBase64String(password);
+            var decrypted = ProtectedData.Unprotect(data, null, DataProtectionScope.LocalMachine);
+            return Encoding.Unicode.GetString(decrypted);
+        }
+
+        public async Task<string> LoginAsync(string email, string password)
+        {
+            var user = await FindUserAsync(email);
+            if (user == null)
+                throw new AuthorizeException("Unauthorized", HttpStatusCode.Unauthorized);
+            if (password != Decrypt(user.Password))
+                throw new AuthorizeException("Password is not correct", HttpStatusCode.Unauthorized);
+            else
+                return GetToken(user);
+        }
+
+        public async Task RegistrationAsync(UserModel model)
+        {
+            if (model.IsModelInvalid())
+                throw new InvalidModelException("uncorrect user model", HttpStatusCode.BadRequest);
+            if (model.Email.IsEmailAlreadyExist(context))
+                throw new ResourceAlreadyExistException("Email already exist", HttpStatusCode.BadRequest);
+
+            var user = mapper.Map<User>(model);
+            user.Role = Roles.Programmer;
+            user.Password = Encrypt(model.Password);
+            await context.ProgrammerRepository.AddAsync(new Programmer { Id = user.Id, User = user });
+            await context.SaveAsync();
+        }
+
+        private async Task<UserModel> FindUserAsync(string email)
+        {
+            return mapper.Map<UserModel>(await context.UserRepository.GetByEmailAsync(email));
+        }
+
+        private string GetToken(UserModel user)
+        {
+            var authParams = authOptions.Value;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = authParams.GetSymmetricSecurityKey();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddSeconds(authParams.TokenLifeTime),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private async Task AddDefaultAdmin()
+        {
+            if (await context.UserRepository.GetOneAsync(u => u.Role == Roles.Administrator) is null)
+            {
+                await context.UserRepository.AddAsync(new User
+                { Name = "admin", Surname = "admin", Email = "KnowledgeAccountingAdmin@gmail.com", Password = Encrypt("admin"), Role = Roles.Administrator });
+                await context.SaveAsync();
+            }
+        }
+    }
 }
--- KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Services/ProgrammerService.cs
+++ KnowledgeAccountingSystem/KnowledgeAccountingSystem.BLL/Services/ProgrammerService.cs
@@ -111,4 +111,103 @@ namespace KnowledgeAccountingSystem.BLL.Services
             await context.SaveAsync();
         }
     }
+
+    public class CopyOfProgrammerService : IProgrammerService
+    {
+        private readonly IUnitOfWork context;
+        private readonly IMapper mapper;
+        public CopyOfProgrammerService(IUnitOfWork context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public void AddSkill(int programmerId, SkillModel skill)
+        {
+            var programmer = context.ProgrammerRepository.GetByIdAsync(programmerId);
+            if (programmer == null)
+                throw new KnowledgeAccountException("Programmer not found", HttpStatusCode.NotFound);
+            if (skill.IsSkillModelNotValid())
+                throw new InvalidModelException("Uncorrect SkillModel", HttpStatusCode.BadRequest);
+            if (skill.IsSkillExist(context, programmerId))
+                throw new ResourceAlreadyExistException("This skill already exists");
+
+            skill.ProgrammerId = programmerId;
+            context.SkillRepository.Add(mapper.Map<Skill>(skill));
+            context.Save();
+        }
+
+        public async Task DeleteAccountAsync(int id)
+        {
+            var user = (await context.ProgrammerRepository.GetByIdAsync(id)).User;
+            await context.ProgrammerRepository.DeleteByIdAsync(id);
+            await context.UserRepository.DeleteByIdAsync(user.Id);
+            await context.SaveAsync();
+        }
+
+        public async Task DeleteSkillAsync(int programmerId, int skillId)
+        {
+            var programmer = await context.ProgrammerRepository?.GetByIdAsync(programmerId);
+            if (programmer == null)
+                throw new KnowledgeAccountException("Programmer not found", HttpStatusCode.NotFound);
+            if (!programmer.Skills.Select(x => x.Id).Contains(skillId))
+                throw new ResourceAlreadyExistException("Skill not found", HttpStatusCode.BadRequest);
+
+            await context.SkillRepository.DeleteByIdAsync(skillId);
+            await context.SaveAsync();
+        }
+
+        public async Task EditSkillAsync(int programmerId, SkillModel skill)
+        {
+            if (!skill.IsSkillExist(context, programmerId))
+                throw new ResourceAlreadyExistException("Programmer don`t have this skill");
+            if (skill.IsSkillModelNotValid())
+                throw new InvalidModelException("Uncorrect SkillModel", HttpStatusCode.BadRequest);
+
+            skill.ProgrammerId = programmerId;
+            var sk = await context.SkillRepository.GetByIdAsync(skill.Id);
+            context.SkillRepository.Update(mapper.Map(skill, sk));
+
+            await context.SaveAsync();
+        }
+
+        public async Task<SkillModel> GetProgrammerSkillByIdAsync(int programmerId, int skillId)
+        {
+            var skills = await GetSkillsAsync(programmerId);
+            SkillModel skill = skills.FirstOrDefault(x => x.Id == skillId);
+
+            if (skill == null)
+                throw new InvalidModelException("Skill not found", HttpStatusCode.BadRequest);
+
+            return skill;
+        }
+
+        public int GetRoleId(int userId)
+        {
+            int? roleId = context.ProgrammerRepository.FindAll().FirstOrDefault(x => x.User.Id == userId)?.Id;
+            if (!roleId.HasValue)
+                throw new AuthorizeException("Unauthorized on this role", HttpStatusCode.Unauthorized);
+
+            return roleId.Value;
+        }
+
+        public async Task<IEnumerable<SkillModel>> GetSkillsAsync(int programmerId)
+        {
+            return mapper.Map<IEnumerable<SkillModel>>((await Task.Run(() =>
+                context.SkillRepository.GetAllProgrammersSkillsById(programmerId)))
+                .AsEnumerable());
+        }
+
+        public async Task UpdateAccountAsync(UserModel model)
+        {
+            if (model.Id.IsAccountNotExist(context))
+                throw new AuthorizeException("No programmers with same id!", HttpStatusCode.NotFound);
+            if (model.IsModelInvalid())
+                throw new InvalidModelException("Uncorrect user model", HttpStatusCode.BadRequest);
+
+            var user = await context.UserRepository.GetByIdAsync(model.Id);
+            context.UserRepository.Update(mapper.Map(model, user));
+            await context.SaveAsync();
+        }
+    }
 }
